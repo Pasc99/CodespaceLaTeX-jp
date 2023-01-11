7697e952b226f4d9b98431dc2e8eecd65c6061ae
pasca <24910213+Pasc99@users.noreply.github.com>
2023-01-11 15:22:27 +0900

    Update msfont.sh

--- msfont.sh
+++ msfont.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 mkdir -p /usr/local/texlive/texmf-local/fonts/map/dvipdfmx/ms
 cp -i ./map/ptex-ms.map /usr/local/texlive/texmf-local/fonts/map/dvipdfmx/ms
-mkdir -p /usr/local/texlive/texmf-local/fonts/truetype/ms
-ln -s ./map/msmincho.ttc /usr/local/texlive/texmf-local/fonts/truetype/ms/msmincho.ttc
-ln -s ./map/msgothic.ttc /usr/local/texlive/texmf-local/fonts/truetype/ms/msgothic.ttc
+mkdir -p /usr/local/texlive/texmf-local/fonts/truetype/local/ms
+cp ./map/msmincho.ttc /usr/local/texlive/texmf-local/fonts/truetype/local/ms/msmincho.ttc
+cp ./map/msgothic.ttc /usr/local/texlive/texmf-local/fonts/truetype/local/ms/msgothic.ttc
+chmod 755 /usr/local/texlive/texmf-local/fonts/truetype/local/ms/msmincho.ttc
+chmod 755 /usr/local/texlive/texmf-local/fonts/truetype/local/ms/msgothic.ttc
 mktexlsr
-updmap-sys --setoption kanjiEmbed ms
+updmap-sys --setoption jaEmbed ms
 mktexlsr
 kanji-config-updmap-sys ms
